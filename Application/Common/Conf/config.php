3aa6cc3d2301a82bac948b0de4d06854a4812068
明炜 卢 <307958040@qq.com>
2020-06-04 22:25:53 +0800

    数据库

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,17 +1,17 @@
 <?php
-/*return array(
+return array(
     //'配置项'=>'配置值'
     'DB_TYPE'=>'mysql',      //数据库类型
     'DB_HOST'=>'111.229.216.178',  //服务器地址
-    'DB_NAME'=>'db_pursey',       //数据库名
+    'DB_NAME'=>'db_student',       //数据库名
     'DB_USER'=>'root',       //数据库账号
     'DB_PWD'=>'@Lumingwei666',      //数据库密码
     'DB_PORT'=>3306,         //数据库端口
-    'DB_PREFIX'=>'ta_',      //数据库表前缀
+    'DB_PREFIX'=>'s_',      //数据库表前缀
     'DB_CHARSET'=>'utf8',    //数据库字符编码
     'URL_MODEL'=>0
-);*/
-return array(
+);
+/*return array(
 	//'配置项'=>'配置值'
     'DB_TYPE'=>'mysql',      //数据库类型
     'DB_HOST'=>'127.0.0.1',  //服务器地址
@@ -22,6 +22,6 @@ return array(
     'DB_PREFIX'=>'s_',      //数据库表前缀
     'DB_CHARSET'=>'utf8',    //数据库字符编码
     'URL_MODEL'=>0
-);
+);*/
 
 
